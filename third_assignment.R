0d7866a2067f8e114d12fca8f66f8be0022f4035
romozzi8 <Peter@MacBook-Pro-di-Pietro-Romozzi.local>
2014-11-06 18:17:22 +0100

    update dataset
    Relevant steps forward in the dataset building, added two more datasets
    to the Rscript, before they have been cleaned, then the original cvs
    files were uploaded in the repo.

--- third_assignment.R
+++ third_assignment.R
@@ -1,9 +1,19 @@
-# Firstly, we set the wd
+## Firstly, we set the wd
 
 setwd("Desktop/Hertie/1st Semester/Collaborative Social Science Data Analysis/ThirdAssignment/")
 
 # We are about to prepare the dataset on which we will carry on our analysis.
-# After having made our excel data about weather tidy, we have converted them in csv format.
-# The next step is to import the data we have prepared before.
+
+## After having made our excel data about weather tidy, we have converted them in csv format.
+## The next step is to import the data we have prepared before.
 
 weather <- read.csv("TidyData_meteo.csv", header = TRUE, sep = ";", dec = ",", fill = TRUE)
+
+## Then, we also load other data needed: the gini index calculated for the macro area considered. Again, we previously have cleaned the datas on excel.
+
+gini <- read.csv("TidyData_gini.csv", header = T, sep =";", dec = ",", fill = T)
+
+## Loading data for suicides (number of suicides per year).
+
+suicides <- read.csv("TidyData_suicides.csv", header = T, sep =";", dec = ",", fill = T)
+
